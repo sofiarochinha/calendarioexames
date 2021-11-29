e706179a0aa5f266b7464f92b043e9092fa942b7
Sofia <s.rocha@ua.pt>
2021-11-29 17:58:50 +0000

    Colocação do template (com rotas)

--- calendario/routes/web.php
+++ calendario/routes/web.php
@@ -12,7 +12,37 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/calendarios', function () {
+    return view('welcome');
+});
+
+Route::get('/calendarios/ano-letivo', function () {
+    return view('welcome');
+});
+
+Route::get('/calendarios/curso', function () {
+    return view('welcome');
+});
+
+Route::get('/configuracoes/disciplinas', function () {
+    return view('welcome');
+});
+
+Route::get('/configuracoes/salas', function () {
+    return view('welcome');
+});
+
+Route::get('/configuracoes/docentes', function () {
+    return view('welcome');
+});
+
+Route::get('/exportar', function () {
+    return view('welcome');
+});
+
+
+
