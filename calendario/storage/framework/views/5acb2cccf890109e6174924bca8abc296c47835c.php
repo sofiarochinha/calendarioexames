95228035b7879590b7b5162894adac5386fe798d
Leon2505 <50246306+Leon2505@users.noreply.github.com>
2021-12-08 16:22:32 +0000

    Grelha por preencher e guiões v2

---
+++ calendario/storage/framework/views/5acb2cccf890109e6174924bca8abc296c47835c.php
@@ -0,0 +1,130 @@
+
+<?php $__env->startSection('conf_disciplinas'); ?>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Salas</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="/calendarios">Home</a></li>
+                            <li class="breadcrumb-item"><a href="#">Configurações</a></li>
+                            <li class="breadcrumb-item active">Salas</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+                <div class="container-fluid">
+                    <div class="row d-flex justify-content-center">
+                        <!-- /.col -->
+                        <div class="col-md-11">
+                            <div class="card card-primary">
+                                <div class="card-body p-0">
+                                    <table class="table-bordered col-md-12">
+                                        <thead>
+                                            <tr class="text-center">
+                                                <th class="col-md-3">Sala</th>
+                                                <th class="col-md-5">Tipo</th>
+                                                <th class="col-md-3">Lotação máxima</th>
+                                            </tr>
+                                        </thead>
+
+                                        <tbody>
+                                            <tr>
+                                                <td class="col-md-3"><input type="text" class="text-center" style="border:0; opacity:0.7; width:100%;" value="5.1.15" disabled/></td>
+                                                <td class="col-md-5"><select style="border:0; width:100%;" disabled/><option class="text-center">Informática</option></td>
+                                                <td class="col-md-3"><input type="select" class="text-center" style="border:0; opacity:0.7; width:100%;" value="50" disabled/></td>
+                                                <td class="col-md-1 text-center"><i class="fas fa-edit" style="cursor:pointer;"></i></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td class="col-md-3">&nbsp</td>
+                                                <td class="col-md-5"></td>
+                                                <td class="col-md-3"></td>
+                                                <td class="col-md-1"></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td class="col-md-3">&nbsp</td>
+                                                <td class="col-md-5"></td>
+                                                <td class="col-md-3"></td>
+                                                <td class="col-md-1"></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td class="col-md-3">&nbsp</td>
+                                                <td class="col-md-5"></td>
+                                                <td class="col-md-3"></td>
+                                                <td class="col-md-1"></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td class="col-md-3">&nbsp</td>
+                                                <td class="col-md-5"></td>
+                                                <td class="col-md-3"></td>
+                                                <td class="col-md-1"></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td class="col-md-3">&nbsp</td>
+                                                <td class="col-md-5"></td>
+                                                <td class="col-md-3"></td>
+                                                <td class="col-md-1"></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td class="col-md-3">&nbsp</td>
+                                                <td class="col-md-5"></td>
+                                                <td class="col-md-3"></td>
+                                                <td class="col-md-1"></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td class="col-md-3">&nbsp</td>
+                                                <td class="col-md-5"></td>
+                                                <td class="col-md-3"></td>
+                                                <td class="col-md-1"></td>
+                                            </tr>
+
+                                            <tr>
+                                                <td class="col-md-3">&nbsp</td>
+                                                <td class="col-md-5"></td>
+                                                <td class="col-md-3"></td>
+                                                <td class="col-md-1"></td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <!-- /.card-body -->
+                            </div>
+                            <!-- /.card -->
+                        </div>
+                        <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </section>
+        <!-- /.content -->
+
+    </div>
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
+        <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+        All rights reserved.
+        <div class="float-right d-none d-sm-inline-block">
+            <b>Version</b> 3.2.0-rc
+        </div>
+    </footer>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layout.menu', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\HP\Documents\GitHub\calendarioexames\calendario\resources\views/conf_salas.blade.php ENDPATH**/ ?>
---
+++ calendario/storage/framework/views/7f8a519d02d5ee5d8a8397f57cb644cc849400db.php
@@ -0,0 +1,250 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Dashboard</title>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="/plugins/jqvmap/jqvmap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="/dist/css/adminlte.min.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href="/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="/plugins/daterangepicker/daterangepicker.css">
+    <!-- summernote -->
+    <link rel="stylesheet" href="/plugins/summernote/summernote-bs4.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+
+
+<!-- Preloader -->
+<div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+</div>
+
+
+<!-- Navbar -->
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+    </ul>
+</nav>
+<!-- /.navbar -->
+
+
+
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a class="brand-link">
+        <img src="/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <span class="brand-text font-weight-light">Ua Calendar</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <a href="#" class="d-block">Username</a>
+            </div>
+        </div>
+
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+                     with font-awesome or any other icon font library -->
+                <li class="nav-item menu-open">
+                    <a class="nav-link active">
+                        <i class="nav-icon fas fa-calendar-alt"></i>
+                        <p>
+                            Calendários
+                            <i class="right fas fa-angle-left"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="/calendarios/atual" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Atual</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/calendarios/historico" class="nav-link active">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Histórico</p>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-copy"></i>
+                        <p>
+                            Configurações
+                            <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <li class="nav-item">
+                            <a href="/configuracoes/disciplinas" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Disciplinas</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/configuracoes/salas" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Salas</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/configuracoes/docentes" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
+                                <p>Docentes</p>
+                            </a>
+                        </li>
+
+                    </ul>
+                </li>
+
+                <li class="nav-item">
+                    <a href="/exportar" class="nav-link">
+                        <i class="nav-icon far fa-image"></i>
+                        <p>
+                            Exportar
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="/" class="nav-link">
+                        <i class="nav-icon far fa-image"></i>
+                        <p>
+                            Sair
+                        </p>
+                    </a>
+                </li>
+
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
+<?php $__env->startSection('curso'); ?>
+<?php echo $__env->yieldSection(); ?>
+
+<?php $__env->startSection('ano_letivo'); ?>
+<?php echo $__env->yieldSection(); ?>
+
+<?php $__env->startSection('sidebar'); ?>
+<?php echo $__env->yieldSection(); ?>
+<?php $__env->startSection('navbar'); ?>
+<?php echo $__env->yieldSection(); ?>
+
+<?php $__env->startSection('configurar'); ?>
+<?php echo $__env->yieldSection(); ?>
+
+<?php $__env->startSection('calendario'); ?>
+<?php echo $__env->yieldSection(); ?>
+
+<?php $__env->startSection('conf_disciplinas'); ?>
+<?php echo $__env->yieldSection(); ?>
+
+<?php $__env->startSection('importar'); ?>
+<?php echo $__env->yieldSection(); ?>
+
+<!-- jQuery -->
+<script src="/plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="/plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+    $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- Sparkline -->
+<script src="/plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="/plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="/plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="/plugins/moment/moment.min.js"></script>
+<script src="/plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="/plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="/dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="/dist/js/pages/dashboard.js"></script>
+
+<!-- Ekko Lightbox -->
+<script src="/plugins/ekko-lightbox/ekko-lightbox.min.js"></script>
+<!-- AdminLTE App -->
+<script src="/dist/js/adminlte.min.js"></script>
+<!-- Filterizr-->
+<script src="/plugins/filterizr/jquery.filterizr.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+
+<!-- DataTables  & Plugins -->
+<script src="/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="/plugins/jszip/jszip.min.js"></script>
+<script src="/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
+<!-- PAGE PLUGINS -->
+<!-- jQuery Mapael -->
+<script src="/plugins/jquery-mousewheel/jquery.mousewheel.js"></script>
+<script src="/plugins/raphael/raphael.min.js"></script>
+<script src="/plugins/jquery-mapael/jquery.mapael.min.js"></script>
+<script src="/plugins/jquery-mapael/maps/usa_states.min.js"></script>
+<!-- ChartJS -->
+<script src="/plugins/chart.js/Chart.min.js"></script>
+
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="/dist/js/pages/dashboard2.js"></script>
+
+
+<!-- jQuery UI -->
+<script src="/plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- fullCalendar 2.2.5 -->
+<script src="/plugins/moment/moment.min.js"></script>
+<script src="/plugins/fullcalendar/main.js"></script>
+
+</html>
+<?php /**PATH C:\Users\HP\Documents\GitHub\calendarioexames\calendario\resources\views/layout/menu.blade.php ENDPATH**/ ?>
