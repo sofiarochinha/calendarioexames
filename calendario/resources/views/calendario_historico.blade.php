0a27121970f8bc10e0114b5f473ba0cab95683ce
Sofia <s.rocha@ua.pt>
2021-12-08 17:59:30 +0000

    Correção do merge na view curso

--- calendario/resources/views/calendario_historico.blade.php
+++ calendario/resources/views/calendario_historico.blade.php
@@ -132,33 +132,7 @@
                             </div>
                             <!-- /.card-body -->
                         </div>
-                        <!-- /.card -->
-                        <!-- <div class="card">
-                          <div class="card-header">
-                            <h3 class="card-title">Create Event</h3>
-                          </div>
-                          <div class="card-body">
-                            <div class="btn-group" style="width: 100%; margin-bottom: 10px;">
-                              <ul class="fc-color-picker" id="color-chooser">
-                                <li><a class="text-primary" href="#"><i class="fas fa-square"></i></a></li>
-                                <li><a class="text-warning" href="#"><i class="fas fa-square"></i></a></li>
-                                <li><a class="text-success" href="#"><i class="fas fa-square"></i></a></li>
-                                <li><a class="text-danger" href="#"><i class="fas fa-square"></i></a></li>
-                                <li><a class="text-muted" href="#"><i class="fas fa-square"></i></a></li>
-                              </ul>
-                            </div>
-                             /btn-group
-                            <div class="input-group">
-                              <input id="new-event" type="text" class="form-control" placeholder="Event Title">
-
-                              <div class="input-group-append">
-                                <button id="add-new-event" type="button" class="btn btn-primary">Add</button>
-                              </div>
-                              /btn-group
-                            </div>
-                            /input-group
-                          </div>
-                        </div> -->
+                       
                     </div>
                 </div>
                 <!-- /.col -->
@@ -181,12 +155,6 @@
 </div>
 <!-- /.content-wrapper -->
 
-<footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-        <b>Version</b> 3.2.0-rc
-    </div>
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-</footer>
 
 <!-- Control Sidebar -->
 <aside class="control-sidebar control-sidebar-dark">
--- calendario/resources/views/curso.blade.php
+++ calendario/resources/views/curso.blade.php
@@ -20,33 +20,7 @@
       </div><!-- /.container-fluid -->
     </section>
 
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-12">
-            <!--<div class="card card-primary">
-              <div class="card-header">
-                <h4 class="card-title">FilterizR Gallery with Ekko Lightbox</h4>
-              </div>-->
-              <div class="card-body">
-                <div>
-                  <div class="filter-container p-0 row">
-                    <div class="filtr-item col-sm-2" data-category="1">
-                      <a href="/calendarios/2020-2021/TI">
-                        <img src="https://via.placeholder.com/300/FFFFFF?text=TI" class="img-fluid mb-2" alt="white sample"/>
-                      </a>
-                    </div>
-                    <div class="filtr-item col-sm-2" data-category="2, 4">
-                    <a href="/calendarios/2020-2021/EMI">
-                        <img src="https://via.placeholder.com/300/FFFFFF?text=EMI" class="img-fluid mb-2" alt="white sample"/>
-                      </a>
-                    </div>
-                </div>
-            </div><!-- /.container-fluid -->
-        </section>
-
-        <!-- Main content -->
+       <!-- Main content -->
         <section class="content">
             <div class="container-fluid">
                 <div class="row">
@@ -70,29 +44,24 @@
                                         </a>
                                     </div>
                                     <div class="filtr-item col-sm-2" data-category="3, 4" data-sort="red sample">
-                                        <a href=/calendarios/atual/{{'text'}} data-toggle="lightbox" data-title="sample 2 - black">
-                                            <img src="https://via.placeholder.com/300/FFFFFF?text=text" class="img-fluid mb-2" alt="white sample"/>
-                                        </a>
+                                    <a href=/calendarios/atual/{{'GC'}}>
+                                        <img src="https://via.placeholder.com/300/FFFFFF?text=GC" class="img-fluid mb-2" alt="white sample"/>
+                                    </a>
                                     </div>
                                     <div class="filtr-item col-sm-2" data-category="2, 4" data-sort="black sample">
-                                        <a href=/calendarios/atual/{{'text'}} data-toggle="lightbox" data-title="sample 2 - black">
-                                            <img src="https://via.placeholder.com/300/FFFFFF?text=text" class="img-fluid mb-2" alt="white sample"/>
-                                        </a>
+                                    <a href=/calendarios/atual/{{'GQ'}}>
+                                        <img src="https://via.placeholder.com/300/FFFFFF?text=GQ" class="img-fluid mb-2" alt="white sample"/>
+                                    </a>
                                     </div>
                                     <div class="filtr-item col-sm-2" data-category="1" data-sort="white sample">
-                                        <a href=/calendarios/atual/{{'text'}} data-toggle="lightbox" data-title="sample 2 - black">
-                                            <img src="https://via.placeholder.com/300/FFFFFF?text=text" class="img-fluid mb-2" alt="white sample"/>
-                                        </a>
+                                    <a href=/calendarios/atual/{{'GP'}}>
+                                        <img src="https://via.placeholder.com/300/FFFFFF?text=GP" class="img-fluid mb-2" alt="white sample"/>
+                                    </a>
                                     </div>
                                     <div class="filtr-item col-sm-2" data-category="1" data-sort="white sample">
-                                        <a href=/calendarios/atual/{{'text'}} data-toggle="lightbox" data-title="sample 2 - black">
-                                            <img src="https://via.placeholder.com/300/FFFFFF?text=text" class="img-fluid mb-2" alt="white sample"/>
-                                        </a>
-                                    </div>
-                                    <div class="filtr-item col-sm-2" data-category="2, 4" data-sort="black sample">
-                                        <a href=/calendarios/atual/{{'text'}} data-toggle="lightbox" data-title="sample 2 - black">
-                                            <img src="https://via.placeholder.com/300/FFFFFF?text=text" class="img-fluid mb-2" alt="white sample"/>
-                                        </a>
+                                    <a href=/calendarios/atual/{{'SCE'}}>
+                                        <img src="https://via.placeholder.com/300/FFFFFF?text=SCE" class="img-fluid mb-2" alt="white sample"/>
+                                    </a>
                                     </div>
 
                                 </div>
@@ -100,51 +69,7 @@
                         </div>
                         <!--</div>-->
                     </div>
-                    <div class="filtr-item col-sm-2" data-category="3, 4" data-sort="red sample">
-                    <a href="/calendarios/2020-2021/GC">
-                        <img src="https://via.placeholder.com/300/FFFFFF?text=GC" class="img-fluid mb-2" alt="white sample"/>
-                      </a>
-                    </div>
-                    <div class="filtr-item col-sm-2" data-category="2, 4" data-sort="black sample">
-                      <a href="/calendarios/2020-2021/GQ">
-                        <img src="https://via.placeholder.com/300/FFFFFF?text=GQ" class="img-fluid mb-2" alt="white sample"/>
-                      </a>
-                    </div>
-                    <div class="filtr-item col-sm-2" data-category="1" data-sort="white sample">
-                      <a href="/calendarios/2020-2021/GP">
-                        <img src="https://via.placeholder.com/300/FFFFFF?text=GP" class="img-fluid mb-2" alt="white sample"/>
-                      </a>
-                    </div>
-                    <div class="filtr-item col-sm-2" data-category="1" data-sort="white sample">
-                    <a href="/calendarios/2020-2021/SCE">
-                        <img src="https://via.placeholder.com/300/FFFFFF?text=SCE" class="img-fluid mb-2" alt="white sample"/>
-                      </a>
-                    </div>
-                    <!--<div class="filtr-item col-sm-2" data-category="2, 4" data-sort="black sample">
-                      <a href="https://via.placeholder.com/1200/FFFFFF.png?text=2014-2015" data-toggle="lightbox" data-title="sample 2 - black">
-                        <img src="https://via.placeholder.com/300/FFFFFF?text=2014-2015" class="img-fluid mb-2" alt="white sample"/>
-                      </a>
-                    </div>-->
-                    <!--<div class="filtr-item col-sm-2" data-category="3, 4" data-sort="red sample">
-                      <a href="https://via.placeholder.com/1200/FFFFFF.png?text=2" data-toggle="lightbox" data-title="sample 2 - black">
-                        <img src="https://via.placeholder.com/300/FFFFFF?text=2" class="img-fluid mb-2" alt="white sample"/>
-                      </a>
-                    </div>
-                    <div class="filtr-item col-sm-2" data-category="1" data-sort="white sample">
-                      <a href="https://via.placeholder.com/1200/FFFFFF.png?text=2" data-toggle="lightbox" data-title="sample 2 - black">
-                        <img src="https://via.placeholder.com/300/FFFFFF?text=2" class="img-fluid mb-2" alt="white sample"/>
-                      </a>
-                    </div>
-                    <div class="filtr-item col-sm-2" data-category="1" data-sort="white sample">
-                      <a href="https://via.placeholder.com/1200/FFFFFF.png?text=2" data-toggle="lightbox" data-title="sample 2 - black">
-                        <img src="https://via.placeholder.com/300/FFFFFF?text=2" class="img-fluid mb-2" alt="white sample"/>
-                      </a>
-                    </div>
-                    <div class="filtr-item col-sm-2" data-category="2, 4" data-sort="black sample">
-                      <a href="https://via.placeholder.com/1200/FFFFFF.png?text=2" data-toggle="lightbox" data-title="sample 2 - black">
-                        <img src="https://via.placeholder.com/300/FFFFFF?text=2" class="img-fluid mb-2" alt="white sample"/>
-                      </a>
-                    </div>-->
+                
                   </div>
                 </div>
               </div>
