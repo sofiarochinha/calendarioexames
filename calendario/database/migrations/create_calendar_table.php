2679525d1c2dc60fa8e5764718358e882a1c4695
Rugaliz <37263443+Rugaliz@users.noreply.github.com>
2021-12-26 16:33:18 +0000

    Fix all migration tables having same class name

--- calendario/database/migrations/create_calendar_table.php
+++ calendario/database/migrations/create_calendar_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateCalendarTable extends Migration
 {
     /**
      * Run the migrations.
