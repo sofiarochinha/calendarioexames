0f91df7a0089fcdf66693b639ccaca08033718ac
Rugaliz <37263443+Rugaliz@users.noreply.github.com>
2022-02-08 20:31:17 +0000

    adjusted database diagram and migration for historic calendar

--- calendario/database/migrations/2014_10_12_000006_create_historic_calendar_table.php
+++ calendario/database/migrations/2014_10_12_000006_create_historic_calendar_table.php
@@ -17,15 +17,15 @@ class CreateHistoricCalendarTable extends Migration
         Schema::create('historic_calendar', function (Blueprint $table) {
             $table->id();
             $table->string('calendar_name');
-            $table->string('evaluation_season');
             $table->string('academic_year');
-            $table->date('day');
+            $table->string('evaluation_season');
             $table->string('course');
             $table->integer('course_year');
-            $table->string('subject');
-            $table->string('time_slot');
             $table->date('start_date');
             $table->date('end_date');
+            $table->date('day');
+            $table->string('subject');
+            $table->string('time_slot');
         });
     }
 
